5bf410faf9e94f6d26c984dded55d24e1b1d23fe
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2020-03-16 15:29:49 -0700

    Add Pika Pack

--- src/utilities/comment.ts
+++ src/utilities/comment.ts
@@ -1,19 +1,19 @@
-import { ISection } from "~/models";
-import { scan } from "~/utilities/scan";
+import { ISection } from "../models/section";
+import { scan } from "./scan";
 
 /**
  * @param contents The contents of your JSON file
  * @param selector A function that accepts a section and determines if it should be commented by returning true (comment) or false (don't comment).
  */
 export function comment(
-	contents: string | Buffer,
+	contents: string,
 	selector: (section: ISection) => boolean = () => true
 ): string {
 	const sections = scan(contents);
 
 	const sectionsToComment = sections.filter(selector);
 
-	const lines = contents.toString().split("\n");
+	const lines = contents.split("\n");
 
 	return lines
 		.map((line, i) => {
@@ -23,7 +23,7 @@ export function comment(
 
 			if (section) {
 				const firstLine = lines[section.start];
-				const leadingSpaces = /^(\s*)/.exec(firstLine)?.[0].length ?? 0;
+				const leadingSpaces = /^(\s*)/.exec(firstLine)![0].length;
 
 				return line.replace(new RegExp(`^(\\s{${leadingSpaces}})`), "$1// ");
 			}
