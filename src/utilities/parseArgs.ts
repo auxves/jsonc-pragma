2cd5fc901f668f92ae3acab5c2b43ff288e9783d
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2020-03-16 21:58:11 -0700

    Use reduce instead of Object.fromEntries

--- src/utilities/parseArgs.ts
+++ src/utilities/parseArgs.ts
@@ -3,9 +3,17 @@ import { IArguments } from "../models/arguments";
 export function parseArgs(match: string | null): IArguments {
 	if (!match) return {};
 
-	return Object.fromEntries([
+	const entries = [
 		...new (typeof URLSearchParams === "undefined"
 			? require("url").URLSearchParams
 			: URLSearchParams)(match.replace(/\s(\w*?=)/, "&$1"))
-	]);
+	];
+
+	return entries.reduce(
+		(acc, [k, v]) => ({
+			...acc,
+			[k]: v
+		}),
+		{}
+	);
 }
